c2a2a6ebdb85dfd207ad342fcf162f38e3ce682c
Daniel <Supercap2F@users.noreply.github.com>
2021-03-09 14:21:17 -0600

    Added basic websocket functionality

--- serv/public/functions.js
+++ serv/public/functions.js
@@ -1,3 +1,35 @@
+// websocket setup code
+var client = new WebSocket('ws://localhost:8000/','echo-protocol');
+
+client.onerror = function() {
+	console.log('Connection Error');
+};
+
+client.onopen = function() {
+	console.log("WebSocket Client Connected");
+
+	function sendNumber() {
+		if(client.readyState === client.OPEN) {
+			var number = Math.round(Math.random() * 0xFFFFFF);
+			client.send(number.toString());
+			//setTimeout(sendNumber, 1000);
+		}
+	}
+
+	sendNumber();
+};
+
+client.onclose = function() {
+	console.log('echo-protocol Client Closed');
+}
+
+client.onmessage = function(e) {
+	if (typeof e.data === 'string') {
+		console.log("Received: '" + e.data + "'");
+	}
+}
+
+// menu functionality code
 
 function changeMenu(callingElement) {
 	var mainmenu = document.getElementById("mainmenu");
@@ -9,6 +41,10 @@ function changeMenu(callingElement) {
 	if(callingElement.id == "heatingmenu-element") {
 		mainmenu.style.display = "none";
 		heatingmenu.style.display = "block"
+
+		if(client.readyState === client.OPEN) {
+			client.send("Client has entered the heating menu");
+		}
 		
 	} else if(callingElement.id == "filtermenu-element") {
 		mainmenu.style.display = "none";
@@ -24,6 +60,10 @@ function changeMenu(callingElement) {
 	} else {
 		callingElement.style.display = "none";
 		mainmenu.style.display = "block"
+
+		if(client.readyState === client.OPEN) {
+			client.send("Back button Pressed");
+		}
 	}
 
 
--- serv/webserver.js
+++ serv/webserver.js
@@ -1,5 +1,6 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var WebSocketServer = require('websocket').server;
 
 app.use(express.static('public'));
 
@@ -11,3 +12,40 @@ var server = app.listen(8000, function () {
     console.log('Express app listening at http://%s:%s', host, port)
 
 })
+
+// =============================================================
+// websockets setup under this line 
+wsServer = new WebSocketServer({
+	httpServer: server,
+	autoAcceptConnections: false
+});
+
+function originIsAllowed(origin) {
+	// put logic here to detech whether the speicified origin is allowed
+	return true;
+}
+
+// this code simply echos anything sent to the server
+wsServer.on('request',function(request){
+	if(!originIsAllowed(request.origin)) {
+		// make sure we only accept requests from allowed origins
+		request.reject();
+		console.log((new Date())+ ' Connection from origin ' + request.origin + ' rejected.');
+		return;
+	}
+
+	var connection = request.accept(('echo-protocol'), request.origin);
+	console.log((new Date()) + ' Connection accpeted.');
+	connection.on('message', function(message) {
+		if(message.type === 'utf8') {
+			console.log(('Recieved Message: ') + message.utf8Data);
+			connection.sendUTF(message.utf8Data);
+		}
+		else if(message.type === 'binary') {
+			console.log('Recieved Binary Message of ' + message.binaryData.length + ' bytes');
+			connection.sendBytes(message.binaryData);
+		}
+	});
+
+});
+
