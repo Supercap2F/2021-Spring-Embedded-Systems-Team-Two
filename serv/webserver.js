7383bee06d63c2e5c5dc2cbd47153306e0516d04
Daniel <Supercap2F@users.noreply.github.com>
2021-04-08 10:22:02 -0500

    Hardware Fully working

--- serv/webserver.js
+++ serv/webserver.js
@@ -2,6 +2,10 @@ var express = require('express');
 var ws = require('ws');
 var gpiop = require("onoff").Gpio;
 const sensor = require('ds18b20-raspi');
+//const spi = require('spi-device');
+const SPI = require('pi-spi');
+
+
 //const r1wt = require('raspi-1wire-temp');
 //const devices = r1wt.findDevices();
 //console.log(devices);
@@ -74,5 +78,81 @@ function turnGPIOoff(pin) {
 }
 
 
-// Temperature sensor setup ===============================
+// Ph and Water level sensor setup
+spi = SPI.initialize("/dev/spidev0.0");
+
+message =  Buffer.from([0b00011110]);
+message2 = Buffer.from([0b00011010]);
+
+spi.clockSpeed(20000);
+
+
+var intervalId2 = setInterval(function() {
+	spi.transfer(message, 2, function (e,d) {
+    	if (e) console.error(e);
+    	else {
+			console.log("Got \""+ d[1] +"\" back.");
+    	} 
+
+	});
+
+
+	spi.transfer(message2, 2, function (e,d) {
+    	if (e) console.error(e);
+    	else {
+			console.log("Got \""+ d[1] +"\" back.");
+    	} 
+
+	});
+}, 2000);
+
+
+
+
+
+
+
+
+
+
+/*
+// The LTC1098 is on bus 0 and it's device 0
+ops = [{
+	threeWire: false
+}];
+const LTC1098 = spi.open(0, 0, ops, err => {
+  // An SPI message is an array of one or more read+write transfers
+
+  // data to start transfer
+  // 0000 - four zeros
+  // 1   - start bit
+  // 1   - select two channel mode
+  // 0/1 - channel 1/2
+  // 1   - MSB first
+  // the LTC now has control but there is a null clock cycle
+  //  - imediatly switch to input mode
+
+});
+
+
+var intervalId2 = setInterval(function() {
+	const message = [{
+	    sendBuffer: Buffer.from([0b00001111]), // Sent to read channel 5
+	    receiveBuffer: Buffer.alloc(1),              // Raw data read from channel 5
+	    byteLength: 1,
+	    speedHz: 20000, // Use a low bus speed to get a good reading from the TMP36
+	  	chipSelectChange: false
+	  }];
+
+	  //if (err) throw err;
+
+	  LTC1098.transfer(message, (err, message) => {
+	    if (err) throw err;
+
+	    console.log('Recieved: ' + message[0].receiveBuffer[0]); // display message read back
+	  });
+
+}, 2000);
+*/
+
 
